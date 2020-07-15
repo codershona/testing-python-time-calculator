ed8e76043f5711dfa143a4e168625794b6f7c61a
Coder--Shona <falgunimst95@gmail.com>
2020-07-15 18:27:35 +0000

    testing case is failing

--- time_calculator.py
+++ time_calculator.py
@@ -1,21 +1,45 @@
-def add_time(start, duration):
+def add_time(start, duration, day='today'):
+
+  def set_ampm(hour):
+    if hour < 12:
+      ampm == 'AM'
+    else:
+      ampm == 'PM'
+
+  startDay = day.islower()
+
+  p = start.replace(':',' ')
+  p = p.split(' ')
+  startHour = int(p[0])
+  startMinute = int(p[1])
+  ampm = p[2]
+
+  r = duration.split(':')
+  addHour = int(r[0])
+  addMinute = int(r[1])
+  hours = 0
+  minutes = 0
+  daysLater = 0
+
+  totalHour = startHour + addHour
+
+ 
+  if totalHour < 24:
+    if totalHour < 12:
+      hours = totalHour
+      set_ampm(totalHour)
+    elif totalHour >= 12:
+      hours = totalHour - 12
+      set_ampm(totalHour)
+
+  if startMinute + addMinute < 60:
+    minutes = startMinute + addMinute
+    # return str(minutes)
+  else:
+    minutes = startMinute + addMinute - 60
+    hours += 1
+    # return str(hours) + ': ' + str(minutes)
+
+  new_time = str(hours) + ':' + '%02d' % minutes + ' ' + ampm
   
   return new_time
-
-    # print("5.add_time")
-    #print(add_time("11:06 PM", "2:02"))
-  print("new_time")
- 
-    
-new_time = input("Enter choice new time:")
- 
-
-num1 = int(input("Enter current time: "))
-num2 = int(input("Enter the continuous time: "))
-
-if new_time == '1':
- new_time = (num1 + num2)
- # print("\nNumber 1 + Number 2 =", answer)
- print(num1, "+", num2, "=", add_time(num1,num2))
-else:
-  print("Invalid number")
